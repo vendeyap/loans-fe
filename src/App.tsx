bedc1ff75519d10724759e8452f1461399311b96
Jorge Eduardo Gil Roa <jorgegildev@gmail.com>
2023-10-22 02:29:35 -0500

    remve datatable and new changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,21 @@ import Header from "./components/header/header";
 import './app.css'
 import {RouterProvider} from "react-router-dom";
 import {routes} from "./routes";
+import {PrimeReactProvider} from "primereact/api";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import 'primeicons/primeicons.css';
+import '/node_modules/primeflex/primeflex.css';
 
 const App = () => {
     return (
+        <PrimeReactProvider>
     <div>
         <Header />
         <div className={'container'}>
             <RouterProvider router={routes} />
         </div>
     </div>
+        </PrimeReactProvider>
   );
 }
 
--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,12 +1,15 @@
 import React, {useEffect, useState} from 'react';
-import DataTable from 'react-data-table-component';
 import './styles.css'
 import {Link} from "react-router-dom";
 import axios from "axios";
+import {Button} from "primereact/button";
+import {Card} from "primereact/card";
+import {DataTable} from "primereact/datatable";
+import {Column} from "primereact/column";
 
 const Home = () => {
 
-    const [data, setData] = useState([])
+    const [payments, setPayments] = useState([])
 
     const getPayments = async () => {
         const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/payment`);
@@ -17,11 +20,12 @@ const Home = () => {
                 month: 'Octubre',
                 paymentDate: '10/01/2023',
                 value: item.amount,
-                outstandingBalance: '$ 42.000.000',
+                outstandingBalance: 42000000,
                 statusPayment: 'Validado',
+                supportImage: 'https://loan-bck.s3.us-east-2.amazonaws.com/supports/vende-ya.png',
             }
         })
-        setData(paymentsList);
+        setPayments(paymentsList);
     }
 
     useEffect(() => {
@@ -29,63 +33,60 @@ const Home = () => {
         getPayments()
     }, []);
 
-    const columns = [
-        {
-            name: 'No de pago',
-            selector: (row) => row.id,
-        },
-        {
-            name: 'Año',
-            selector: (row) => row.year,
-        },
-        {
-            name: 'Mes',
-            selector: (row) => row.month,
-        },
-        {
-            name: 'Fecha de pago',
-            selector: (row) => row.paymentDate,
-        },
-        {
-            name: 'Valor',
-            selector: (row) => row.value,
-        },
-        {
-            name: 'Saldo pendiente',
-            selector: (row) => row.outstandingBalance,
-        },
-        {
-            name: 'Estado del pago',
-            selector: (row) => row.statusPayment,
-        },
-        {
-            name: 'Acciones',
-            selector: (row) => row.statusPayment,
-        },
-    ];
+    const formatCurrency = (value) => {
+        return value.toLocaleString('es-CO', { style: 'currency', currency: 'COP' });
+    };
+
+    const imageBodyTemplate = (payment) => {
+        return <a href={payment.supportImage} target={'_blank'} rel="noreferrer">
+            <img src={`${payment.supportImage}`} alt={payment.statusPayment} className="w-6rem shadow-2 border-round" />
+        </a> ;
+    };
+
+    const valueBodyTemplate = (payment) => {
+        return formatCurrency(payment.value);
+    };
+    const outstandingBodyTemplate = (payment) => {
+        return formatCurrency(payment.outstandingBalance);
+    };
+
 
     return (
-        <div className={'home'}>
+        <>
+            <Card title="Informe de pago" subTitle={'Resumen de pagos para prestamo vehicular'} className={'mt-5'}>
 
-            <div className={'money-content text-center'}>
-                <span className={'money-title'}> Saldo total a la fecha </span>
-                <span className={'money-price'}> $ 44.000.000</span>
-            </div>
+                <div className={'flex'}>
+                    <div className={'money-content text-center'}>
+                        <span className={'text-2xl'}> Total de prestamo </span>
+                        <span className={'text-4xl font-light pt-5'}> $ 44.000.000</span>
+                    </div>
 
-            <div className={'button-content'}>
-                <Link to={'/nuevo-pago'} className={'button-generic'}>
-                    Registrar Pago
-                </Link>
-            </div>
+                    <div className={'money-content text-center'}>
+                        <span className={'text-2xl'}> Saldo a la fecha </span>
+                        <span className={'text-4xl font-light pt-5'}> $ 42.000.000</span>
+                    </div>
+                </div>
 
-            <div className={'table-content'}>
-                <DataTable
-                    columns={columns as any}
-                    data={data}
-                />
-            </div>
 
-        </div>
+                <div className={'flex justify-content-end py-4'}>
+                    <Link to={'/nuevo-pago'}>
+                        <Button label="Registrar pago" link icon="pi pi-money-bill" />
+                    </Link>
+                </div>
+
+                <div className={'flex-row'}>
+                    <div className="card">
+                        <DataTable value={payments} tableStyle={{ minWidth: '50rem' }}>
+                            <Column field="paymentDate" header="Fecha de pago"></Column>
+                            <Column field="value" header="Valor" body={valueBodyTemplate}></Column>
+                            <Column field="outstandingBalance" header="Saldo restante" body={outstandingBodyTemplate}></Column>
+                            <Column field="statusPayment" header="Estado de pago"></Column>
+                            <Column field="supportImage" header="Comprobante" body={imageBodyTemplate}></Column>
+                        </DataTable>
+                    </div>
+                </div>
+            </Card>
+        </>
     );
 };
 
